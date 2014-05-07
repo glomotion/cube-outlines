9bf6dba8159b7f2557fc9efcd2d3460c00122097
Tim Paul <timpaul@google.com>
2014-05-07 20:42:53 +1000

    getting cube with outline working + wrapping a texture on two faces...

--- main.js
+++ main.js
@@ -1,26 +1,67 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
+var geometryT, materialT, meshT;
 
 init();
 animate();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 1000;
-
     scene = new THREE.Scene();
 
-    geometry = new THREE.CubeGeometry(200, 200, 200);
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10);
+    camera.position.set(0, 2, 6);
+    camera.lookAt(scene.position);
+
+    geometry = new THREE.CubeGeometry(2,2,2);
     material = new THREE.MeshBasicMaterial({
-        color: 0xff0000,
-        wireframe: true
+        color: 0xffffff,
+        wireframe: true,
+        wireframeLinewidth: 4,
     });
 
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
-    renderer = new THREE.CanvasRenderer();
+    geometryT = new THREE.CubeGeometry(1.99,1.99,1.99);
+    materialT = [
+        // right start face
+        new THREE.MeshLambertMaterial({
+            map: THREE.ImageUtils.loadTexture('blank.png')
+        }),
+        // left start face
+        new THREE.MeshLambertMaterial({
+            map: THREE.ImageUtils.loadTexture('blank.png')
+        }),
+        // top face:
+        new THREE.MeshLambertMaterial({
+            map: THREE.ImageUtils.loadTexture('blank.png')
+        }),
+        // bottom face?
+        new THREE.MeshLambertMaterial({
+            map: THREE.ImageUtils.loadTexture('blank.png')
+        }),
+        // front face
+        new THREE.MeshLambertMaterial({
+            map: THREE.ImageUtils.loadTexture('logo-face.png'),
+            emissive: 0xffffff
+        }),
+        // back face
+        new THREE.MeshLambertMaterial({
+            map: THREE.ImageUtils.loadTexture('logo-face.png'),
+            emissive: 0xffffff
+        }),
+    ];
+
+    meshT = new THREE.Mesh(
+        geometryT,
+        new THREE.MeshFaceMaterial( materialT ) 
+    );
+    scene.add(meshT);
+
+    // renderer = new THREE.CanvasRenderer();
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(renderer.domElement);
@@ -33,7 +74,8 @@ function animate() {
     requestAnimationFrame(animate);
 
     // mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.002;
+    mesh.rotation.y -= 0.002;
+    meshT.rotation.y -= 0.002;
 
     renderer.render(scene, camera);
 
